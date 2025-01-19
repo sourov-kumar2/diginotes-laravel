58f3305e7bb13b0cf049a6929f7b62ebd555fbda
sourov-kumar2 <sourovmail082@gmail.com>
2025-01-19 17:54:46 +0600

    added mastering and multi auth system

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.admin')
+@section('title', 'Dashboard')
+@section('content')
+    <h4>This is a Dashboard</h4>
+@endsection
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title')</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="{{ asset('assets/admin/css/style.css') }}" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body id="page-top">
+    <div class="wrapper d-flex">
+
+        @include('layouts.partials.sidebar')
+
+        <div id="content-wrapper" class="d-flex flex-column flex-grow-1">
+            @include('layouts.partials.nav')
+            <div id="content">
+                <div class="container-fluid">
+                    @yield('content')
+                </div>
+            </div>
+            @include('layouts.partials.footer')
+        </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap4.min.js"></script>
+    <link href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <script src="{{ asset('assets/admin/js/app.js') }}"></script>
+</body>
+</html>
